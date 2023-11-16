37def86a7111f60c6e9e818b39cb59f6282cf9f3
Axxzjavon Sampson <axxzjano@gmail.com>
2023-11-16 09:44:48 -0600

    finished test exercise

--- code.js
+++ code.js
@@ -1,4 +1,24 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+//     // console.log("Hello, World!");
+// }
+
+const helloWorld = () => {
     return "Hello, World!";
+}
+
+function sayHello(input) {
+    // if(input === 'Pat') {
+    //     return "Hello, Pat!";
+    // }
+    // if(input === 'Alex') {
+    //     return "Hello, Alex!";
+    // }
+    // return "Hello, Jane!";
+
+
+    // first refactor
+    return "Hello, " + input + "!";
+    // return `Hello, ${input}!`;
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,36 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+        expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+        expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+        expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+    it('should return "Hello, Jane!" when passed "Jane"', () => {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" when passed "Alex"', () => {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!" when passed "Pat"', () => {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
 });
